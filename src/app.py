5f795d80c0392a7cc3e2d79564e0a88ec83d5811
Adi Levi <adile0194@gmail.com>
2024-08-22 08:08:02 +0300

    feat: added routers

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from mangum import Mangum
-from routers.router import router 
+from routers.artistsRouter import router 
+from routers.chartsRouter import router 
+from routers.genresRouter import router 
+from routers.songsRouter import router 
 import uvicorn
 
 app = FastAPI()
