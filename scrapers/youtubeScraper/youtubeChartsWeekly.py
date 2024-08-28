6d8be02e1c6a3f96ca78d2f2f9a75991043f2fe6
Adi Levi <104142857+adi-git33@users.noreply.github.com>
2024-08-28 20:27:16 +0300

    fixed: env works, now theres a new error

--- scrapers/youtubeScraper/youtubeChartsWeekly.py
+++ scrapers/youtubeScraper/youtubeChartsWeekly.py
@@ -9,34 +9,35 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-def handler(event, context):
-    YOUTUBE_CHARTS_API_KEY  = os.environ.get("YOUTUBE_CHARTS_API_KEY")
-    YOUTUBE_CHARTS_URL_KEY = os.environ.get("YOUTUBE_CHARTS_URL_KEY")
-    YOUTUBE_CHARTS_COOKIE = os.environ.get("YOUTUBE_CHARTS_COOKIE")
+YOUTUBE_CHARTS_API_KEY  = os.environ.get("YOUTUBE_CHARTS_API_KEY")
+YOUTUBE_CHARTS_URL_KEY = os.environ.get("YOUTUBE_CHARTS_URL_KEY")
+YOUTUBE_CHARTS_COOKIE = os.environ.get("YOUTUBE_CHARTS_COOKIE")
 
-    timing = "WEEKLY"
-    youtube_chart = "Youtube Charts"
+timing = "WEEKLY"
+youtube_chart = "Youtube Charts"
 
-    youtube_scraper = YoutubeScraper(YOUTUBE_CHARTS_API_KEY, YOUTUBE_CHARTS_URL_KEY, YOUTUBE_CHARTS_COOKIE)
-    global_charts = youtube_scraper.fetch_charts("global", timing, youtube_chart)
-    countries_charts = youtube_scraper.fetch_all_countries_charts(timing, youtube_chart)
+youtube_scraper = YoutubeScraper(YOUTUBE_CHARTS_API_KEY, YOUTUBE_CHARTS_URL_KEY, YOUTUBE_CHARTS_COOKIE)
+global_charts = youtube_scraper.fetch_charts("global", timing, youtube_chart)
+countries_charts = youtube_scraper.fetch_all_countries_charts(timing, youtube_chart)
 
-    data = []
-    data.extend(global_charts)
-    data.extend(countries_charts)
+data = []
+data.extend(global_charts)
+data.extend(countries_charts)
 
-    sqs = boto3.client('sqs', endpoint_url='http://sqs:9324',region_name='us-east-1',aws_access_key_id='x', aws_secret_access_key='x')
-    queue_url = 'http://sqs:9324/queue/data-raw-q'
-    
-    try:
-        scraped_data = data
-        response = sqs.send_message(
-            QueueUrl=queue_url,
-            MessageBody=json.dumps(scraped_data, ensure_ascii=False)
-        )
-        print({"message": "Data has been scraped and sent to SQS", "sqs_response": response})
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+sqs = boto3.client(
+    'sqs', 
+    region_name="us-east-1",
+    endpoint_url='http://sqs:9324'
+)
 
-if __name__ == "__main__":
-    handler(None, None)
+queue_url = 'http://sqs:9324/queue/data-raw-q'
+
+try:
+    scraped_data = data
+    response = sqs.send_message(
+        QueueUrl=queue_url,
+        MessageBody=json.dumps(scraped_data, ensure_ascii=False)
+    )
+    print({"message": "Data has been scraped and sent to SQS", "sqs_response": response})
+except Exception as e:
+    raise HTTPException(status_code=500, detail=str(e))
