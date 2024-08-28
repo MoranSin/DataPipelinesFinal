9947b9ab0a2275a6aefd0d395c0b9a922939deb8
Adi Levi <104142857+adi-git33@users.noreply.github.com>
2024-08-28 18:30:30 +0300

    fix: trying to fix scheduler

--- scrapers/youtubeScraper/youtubeChartsDaily.py
+++ scrapers/youtubeScraper/youtubeChartsDaily.py
@@ -1,5 +1,5 @@
 import os
-from youtubeScraper import YoutubeScraper
+from .youtubeScraper import YoutubeScraper 
 import boto3
 import json
 from fastapi import HTTPException
@@ -9,43 +9,41 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-YOUTUBE_CHARTS_API_KEY  = os.environ.get("YOUTUBE_CHARTS_API_KEY")
-YOUTUBE_CHARTS_URL_KEY = os.environ.get("YOUTUBE_CHARTS_URL_KEY")
-YOUTUBE_CHARTS_COOKIE = os.environ.get("YOUTUBE_CHARTS_COOKIE")
-YOUTUBE_TRENDS_API_KEY = os.environ.get("YOUTUBE_TRENDS_API_KEY")
-YOUTUBE_TRENDS_COOKIE = os.environ.get("YOUTUBE_TRENDS_COOKIE")
+def handler(event, context):
+    YOUTUBE_CHARTS_API_KEY  = os.environ.get("YOUTUBE_CHARTS_API_KEY")
+    YOUTUBE_CHARTS_URL_KEY = os.environ.get("YOUTUBE_CHARTS_URL_KEY")
+    YOUTUBE_CHARTS_COOKIE = os.environ.get("YOUTUBE_CHARTS_COOKIE")
+    YOUTUBE_TRENDS_API_KEY = os.environ.get("YOUTUBE_TRENDS_API_KEY")
+    YOUTUBE_TRENDS_COOKIE = os.environ.get("YOUTUBE_TRENDS_COOKIE")
 
-timing = "DAILY"
-youtube_chart = "Youtube Charts"
-youtube_trends = "Youtube Trends"
+    timing = "DAILY"
+    youtube_chart = "Youtube Charts"
+    youtube_trends = "Youtube Trends"
 
-youtube_charts_scraper = YoutubeScraper(YOUTUBE_CHARTS_API_KEY, YOUTUBE_CHARTS_URL_KEY, YOUTUBE_CHARTS_COOKIE)
-global_charts = youtube_charts_scraper.fetch_charts("global", timing, youtube_chart)
-countries_charts = youtube_charts_scraper.fetch_all_countries_charts(timing, youtube_chart)
+    youtube_charts_scraper = YoutubeScraper(YOUTUBE_CHARTS_API_KEY, YOUTUBE_CHARTS_URL_KEY, YOUTUBE_CHARTS_COOKIE)
+    global_charts = youtube_charts_scraper.fetch_charts("global", timing, youtube_chart)
+    countries_charts = youtube_charts_scraper.fetch_all_countries_charts(timing, youtube_chart)
 
-data = []
-data.extend(global_charts)
-data.extend(countries_charts)
+    data = []
+    data.extend(global_charts)
+    data.extend(countries_charts)
 
-youtube_trends_scraper = YoutubeScraper(YOUTUBE_CHARTS_API_KEY, YOUTUBE_TRENDS_API_KEY, YOUTUBE_TRENDS_COOKIE)
-trends_data = youtube_charts_scraper.fetch_all_countries_charts(timing, youtube_trends)
+    youtube_trends_scraper = YoutubeScraper(YOUTUBE_CHARTS_API_KEY, YOUTUBE_TRENDS_API_KEY, YOUTUBE_TRENDS_COOKIE)
+    trends_data = youtube_charts_scraper.fetch_all_countries_charts(timing, youtube_trends)
 
-data.extend(trends_data)
+    data.extend(trends_data)
 
+    sqs = boto3.client('sqs', endpoint_url='http://sqs:9324',region_name='us-east-1',aws_access_key_id='x', aws_secret_access_key='x')
+    queue_url = 'http://sqs:9324/queue/data-raw-q'
 
-sqs = boto3.client(
-    'sqs', 
-    region_name="us-east-1",
-    endpoint_url='http://sqs:9324'
-)
+    try:
+        response = sqs.send_message(
+            QueueUrl=queue_url,
+            MessageBody=json.dumps(data, ensure_ascii=False)
+        )
+        print({"message": "Data has been scraped and sent to SQS", "sqs_response": response})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
-queue_url = 'http://sqs:9324/queue/data-raw-q'
-
-try:
-    response = sqs.send_message(
-        QueueUrl=queue_url,
-        MessageBody=json.dumps(data, ensure_ascii=False)
-    )
-    print({"message": "Data has been scraped and sent to SQS", "sqs_response": response})
-except Exception as e:
-    raise HTTPException(status_code=500, detail=str(e))
+if __name__ == "__main__":
+    handler(None, None)
