d0fff2f43a35b2ef1a530ab1ac86e36aa13a0dc4
Adi Levi <104142857+adi-git33@users.noreply.github.com>
2024-09-08 01:32:25 +0300

    fix: changed from mock

--- pulse-app/src/services/chart.service.js
+++ pulse-app/src/services/chart.service.js
@@ -2,22 +2,22 @@ import axios from "axios";
 import { SERVER_BASE_URL } from "../common/consts/api";
 
 export const getChartAvailableDates = async () => {
-  // const response = await axios.get(new URL("/dev/charts/available-dates", SERVER_BASE_URL).toString());
-  const response = await axios.get(new URL("/charts/available-dates", SERVER_BASE_URL).toString());
+  const response = await axios.get(new URL("/dev/charts/available-dates", SERVER_BASE_URL).toString());
+  // const response = await axios.get(new URL("/charts/available-dates", SERVER_BASE_URL).toString());
   console.log("getChartAvailableDates:", response.data);
   return response;
 };
 
 export const getChartByYear = async (year) => {
-  const response = await axios.get(new URL(`/charts?year=${year}`, SERVER_BASE_URL).toString());
-  // const response = await axios.get(new URL(`/dev/charts?year=${year}`, SERVER_BASE_URL).toString());
+  // const response = await axios.get(new URL(`/charts?year=${year}`, SERVER_BASE_URL).toString());
+  const response = await axios.get(new URL(`/dev/charts?year=${year}`, SERVER_BASE_URL).toString());
   console.log("getChartByYear:", response.data);
   return response;
 };
 
 export const getChartByDate = async (date) => {
-  const response = await axios.get(new URL(`/charts?date=${date}`, SERVER_BASE_URL).toString());
-  // const response = await axios.get(new URL(`/dev/charts?date=${date}`, SERVER_BASE_URL).toString());
+  // const response = await axios.get(new URL(`/charts?date=${date}`, SERVER_BASE_URL).toString());
+  const response = await axios.get(new URL(`/dev/charts?date=${date}`, SERVER_BASE_URL).toString());
   console.log("getChartByYear:", response.data);
   return response;
 };
